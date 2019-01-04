728c5d73f884518bad6c7d9d5e60d03660f6bc25
cuiwenju <cuicharles@163.com>
2019-01-04 09:52:37 +0800

    import Libraries and add BinarySearch algorithm

--- src/com/charles/algorithm/Test.java
+++ src/com/charles/algorithm/Test.java
@@ -2,10 +2,16 @@ package com.charles.algorithm;
 
 import com.charles.algorithm.book_utils.StdOut;
 
-public class Test {
+public class Test extends Object{
 
     public static void main(String[] args){
 
-        StdOut.printf("%d is my lucky number.",12);
+        StdOut.printf("%5d is my lucky number.",12);
+        StdOut.println();
+        StdOut.printf("%-5d is my lucky number.",12);
+
+
+        StdOut.println();
+        StdOut.printf(String.valueOf(1.0/0.0));
     }
 }
