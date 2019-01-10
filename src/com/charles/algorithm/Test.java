0f89c28354ffd9b81a502e655e61890d78e83444
cuiwenju <cuicharles@163.com>
2019-01-10 16:34:55 +0800

    Test final keywords

--- src/com/charles/algorithm/Test.java
+++ src/com/charles/algorithm/Test.java
@@ -4,14 +4,20 @@ import com.charles.algorithm.book_utils.StdOut;
 
 public class Test extends Object{
 
+    public static void modify(String s){
+        s += "dd";
+        System.out.println(s);
+    }
+
+
+
     public static void main(String[] args){
 
-        StdOut.printf("%5d is my lucky number.",12);
-        StdOut.println();
-        StdOut.printf("%-5d is my lucky number.",12);
+        char[] s = {'a','b','c'};
+        String a = new String(s);
+        System.out.println(a);
+        s[1] = 's';
+        System.out.println(a);
 
-
-        StdOut.println();
-        StdOut.printf(String.valueOf(1.0/0.0));
     }
 }
