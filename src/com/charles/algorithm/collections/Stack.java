e94f1eac928d4604a44e511cf1069c6f921c585f
cuiwenju <cuicharles@163.com>
2019-01-19 10:12:38 +0800

    Implements and tests a generic Stack with data structure of linked-list

--- src/com/charles/algorithm/collections/Stack.java
+++ src/com/charles/algorithm/collections/Stack.java
@@ -8,46 +8,76 @@ import java.util.function.Consumer;
  * LIFO
  * @param <Item>
  */
-public class Stack<Item> implements Iterable<Item> {
+public class Stack<Item> /*implements Iterable<Item> */{
+
+    private Node first;
+    private int N;
 
 
     public Stack(){
-        //todo
+
     }
 
     public void push(Item item){
-        //todo
+        Node oldFirstNode = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirstNode;
+        N++;
     }
 
     public Item pop(){
-        //todo
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
     }
 
     public int size(){
-        //todo
-        return 0;
+
+        return N;
     }
 
     public boolean isEmpty(){
-        //todo
-        return false;
+        return N == 0;
     }
 
 
+//
+//    @Override
+//    public Iterator<Item> iterator() {
+//        return null;
+//    }
+//
+//    @Override
+//    public void forEach(Consumer<? super Item> action) {
+//
+//    }
+//
+//    @Override
+//    public Spliterator<Item> spliterator() {
+//        return null;
+//    }
 
-    @Override
-    public Iterator<Item> iterator() {
-        return null;
+
+    private class Node{
+        Item item;
+        Node next;
     }
 
-    @Override
-    public void forEach(Consumer<? super Item> action) {
-
-    }
-
-    @Override
-    public Spliterator<Item> spliterator() {
-        return null;
+    public static void main(String[] args){
+        Stack<String> stack = new Stack<>();
+        System.out.println("Stack是否为空："+stack.isEmpty());
+        stack.push("cui");
+        stack.push("wen");
+        stack.push("ju");
+        System.out.println("Stack的数量是："+stack.size());
+        System.out.println("Stack出栈结果：" + stack.pop());
+        System.out.println("Stack的数量是："+stack.size());
     }
 }
+
+
